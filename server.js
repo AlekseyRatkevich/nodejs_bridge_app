b76783772d20c9a2bfbb1c93a4430c5f78e3ebbb
Лёша Летов <maminakyrtka@MacBook-Pro-Lesa-Letov.local>
2022-02-18 13:27:13 +0300

    feat: create unique room id and automaticaly redirect to this id on page loading

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+const { response } = require('express')
 const express = require('express')
+const { request } = require('http')
 const app = express()
 const server = require('http').Server(app)
 const { v4: uuidv4 } = require('uuid')
 
-app.set('view engine', 'ejsf')
+app.set('view engine', 'ejs')
 
 app.get('/', (request, response) => {
-    response.render('room')
+    response.redirect(`/${uuidv4()}`)
 })
 
+app.get('/:room', (request, response) => {
+    response.render('room', { roomId: request.params.room })
+})
 
 server.listen(3000)
